3a59916d02a0cc0e3af892c71c3c339959b3d1d3
max4c <hello@maxforsey.com>
2024-07-26 11:24:18 +0200

    added support for click on toggle

--- page-components-loader.js
+++ page-components-loader.js
@@ -5,29 +5,60 @@ function loadComponent(url, elementId) {
 		if (this.readyState === 4 && this.status === 200) {
 			document.getElementById(elementId).innerHTML = this.responseText;
 			if (elementId === "sidebar") {
-				addHoverInterests();
+				addInterestsInteraction();
 			}
 		}
 	};
 	xhr.send();
 }
 
-function addHoverInterests() {
+function addInterestsInteraction() {
 	const toggleInterests = document.querySelector(".toggle-interests");
 	const interestsContent = document.querySelector(".interests-content");
+	const interestsSection = document.querySelector(".interests-section");
 
 	if (toggleInterests && interestsContent) {
-		toggleInterests.addEventListener("mouseenter", function () {
-			this.classList.add("open");
-			interestsContent.classList.add("visible");
-		});
+		if (isTouchDevice()) {
+			// For touch devices (mobile)
+			toggleInterests.addEventListener("click", function (e) {
+				e.preventDefault();
+				this.classList.toggle("open");
+				if (interestsContent.classList.contains("visible")) {
+					closeInterests(interestsContent);
+				} else {
+					openInterests(interestsContent);
+				}
+			});
+		} else {
+			// For non-touch devices (desktop)
+			toggleInterests.addEventListener("mouseenter", function () {
+				this.classList.add("open");
+				openInterests(interestsContent);
+			});
 
-		const interestsSection = document.querySelector(".interests-section");
-		interestsSection.addEventListener("mouseleave", () => {
-			toggleInterests.classList.remove("open");
-			interestsContent.classList.remove("visible");
-		});
+			interestsSection.addEventListener("mouseleave", () => {
+				toggleInterests.classList.remove("open");
+				closeInterests(interestsContent);
+			});
+		}
 	}
+}
+
+function openInterests(interestsContent) {
+	interestsContent.style.display = "block";
+	setTimeout(() => interestsContent.classList.add("visible"), 50);
+}
+
+function closeInterests(interestsContent) {
+	interestsContent.classList.remove("visible");
+}
+
+function isTouchDevice() {
+	return (
+		"ontouchstart" in window ||
+		navigator.maxTouchPoints > 0 ||
+		navigator.msMaxTouchPoints > 0
+	);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
